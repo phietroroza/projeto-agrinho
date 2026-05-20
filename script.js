b7c74dc3dbb9754b7f4ef144c3ffcdc1221c161b
phietroroza <phietro.roza@escola.pr.gov.br>
2026-05-19 22:04:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function mudaTema() {
     }
 
 }
+let curtidas = 0;
+let dislikes = 0;
+
+function curtir() {
+  curtidas++;
+  document.getElementById("curtidas").innerText = curtidas;
+}
+
+function dislike() {
+  dislikes++;
+  document.getElementById("dislikes").innerText = dislikes;
+}
